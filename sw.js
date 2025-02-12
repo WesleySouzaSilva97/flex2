0f9fff9d444975f67f7166fb62aed27d8ac63d99
WESLEY DE SOUZA DA SILVA <wesley.ssilva48@senacsp.edu.br>
2025-02-11 22:07:05 -0300

    app flex2 finalizado

--- sw.js
+++ sw.js
@@ -3,10 +3,37 @@
  * @author Wesley Souza
  */
 
-// Instalação (cache "armazenamento local /local storage")
-
-
+// Instalação (cache "armazenamento local")
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('static')
+            .then((cache) => {
+                cache.add('/flex2/')
+                cache.add('/flex2/index.html')
+                cache.add('/flex2/style.css')
+                cache.add('/flex2/app.js')
+                cache.add('/flex2/img/flex.png')
+                cache.add('/flex2/img/calcflex.png')
+                cache.add('/flex2/img/etanol.png')
+                cache.add('/flex2/img/gasolina.png')
+            })
+    )
+})
 // Ativação
-
-
-// Interceptação (solicitações https e servindo em cache quando off-line))
+self.addEventListener('activate', (event) => {
+    console.log("Ativando o service worker...", event)
+    return self.clients.claim()
+})
+// Interceptação (solicitações https servindo em cache quando off-line)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                if (response) {
+                    return response
+                } else {
+                    return fetch(event.request)
+                }
+            })
+    )
+})
